c19815c5b20baec9e5bf0277ca8f0535927188a5
allensftort <allenstfort1@gmail.com>
2025-03-02 20:42:35 -0500

    removed api to test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,4 @@
 import "./App.css";
-import { API_URL } from "./config";
-
-async function fetchData() {
-  const response = await fetch(`${API_URL}/your-endpoint`);
-  const data = await response.json();
-  console.log(data);
-}
 
 function App() {
   return (
