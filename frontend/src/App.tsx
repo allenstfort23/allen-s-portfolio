fab4cbdf7137fc5b5d8fe41314ff3866186406f5
allensftort <allenstfort1@gmail.com>
2025-03-08 23:27:42 -0500

    Add components and pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Projects from "./pages/Projects/Projects";
+import Home from "./pages/Home/home";
 
 function App() {
   return (
-    <>
-      <div>Hello World</div>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="projects" element={<Projects />} />
+      </Routes>
+    </Router>
   );
 }
 
